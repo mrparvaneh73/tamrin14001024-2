e39748cf58e8d9ca9dd9e194b77d2248054f2ff9
milad parvaneh <parvanehmilad73@yahoo.com>
2022-01-14 19:45:04 +0330

    main

---
+++ app/src/main/java/com/example/test140010242/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.test140010242
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
